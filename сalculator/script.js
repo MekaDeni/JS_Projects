826e19eef88d84e9cabf7e6d5b015fc9811d721a
Deni <39564779+MekaDeni@users.noreply.github.com>
2019-07-26 12:46:04 +0300

    start buil new project 'canculator'

---
+++ сalculator/script.js
@@ -0,0 +1,16 @@
+'use-strict';
+
+function startApp() {
+	// body...
+	console.log('HELLO WORLD!!!');
+}
+
+function clickButtonNum() {
+	// body...
+}
+
+function clickButtonCommand() {
+	// body...
+}
+
+document.addEventListener("DOMContentLoaded", startApp());
